60957fbc061c3aeadb59ae7f465ad9bf117678bf
Mrsaiba <jitsaiba@gmail.com>
2024-08-03 15:36:56 +0530

    added button

--- app/index.jsx
+++ app/index.jsx
@@ -3,6 +3,7 @@ import { ScrollView, Text, View, Image } from "react-native";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
@@ -19,6 +20,28 @@ export default function App() {
             source={images.cards}
             className="max-w-[380px] w-full h-[300px]"
             resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities With{" "}
+              <Text className="text-secondary-200">Arora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Arora
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => {}}
+            containerStyles="w-full mt-7"
           />
         </View>
       </ScrollView>
